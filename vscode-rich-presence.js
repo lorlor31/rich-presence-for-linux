31db8563a48752a1ed14a656ecaf9d86022cbf0c
lorlor31 <lorlordevweb@gmail.com>
2025-01-30 12:17:29 +0100

    randomActivities

--- vscode-rich-presence.js
+++ vscode-rich-presence.js
@@ -7,9 +7,21 @@ RPC.register(clientId);
 
 client.on('ready', () => {
     console.log('Rich Presence actif pour VSCode !');
+    const randomNumber = Math.floor(Math.random() * 5); // Int entre 0 et 4
+    // Tabl de JSON perso 
+    activities = [
+      { details: "En duel avec le code", state: "Combat des algos terrifiants" },
+      { details: "Gastronomie de code", state: "Démêle un plat de spaghettis" },
+      { details: "Archéologie de code", state: "Retour dans du code legacy" },
+      { details: "Dépannage de code", state: "Répare un code planté " },
+      { details: "Désinsectisation de code", state: "A l'affût des bugs" },
+    ];
+
+
+
     client.setActivity({
-        details: 'Édition de code',
-        state: 'Sur des bugs prises de tête',
+        details: activities[randomNumber]['details'],
+        state: activities[randomNumber]['state'],
         startTimestamp: Date.now(),
         largeImageKey: 'VSCode', // Image que vous téléchargez dans Discord Developer Portal
         largeImageText: 'Visual Studio Code',
